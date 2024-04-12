f7d9d5215063d14c91da8634790215a3afa97dcb
Sujoy Das <sujoykarnel@gmail.com>
2024-04-12 14:24:11 +0600

    projet final

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
 //dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
-const data = require('./lib/data');
-const { sendTwilioSms } = require('./helpers/notifications');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
+
 
 // app object - module scaffolding
 const app = {};
 
-// // test @TODO
-// sendTwilioSms('01737300123', 'Hello World', (err) => {
-//     console.log(`This is the error ${err}`);
-// });
 
-
-// create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        console.log(`listening to post ${environment.port}`);
-    });
+app.init = () => {
+    // start the server
+    server.init();
+    // start the workers
+    workers.init();
 };
 
+app.init();
 
-
-// handle request response
-app.handleReqRes = handleReqRes;
-
-// start the server
-app.createServer();
+// export the app 
+module.exports = app;
